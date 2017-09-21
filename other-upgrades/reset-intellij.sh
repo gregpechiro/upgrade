e124e04f5d26f36d0a7292b47ae8eeac163a91d1
Greg Pechiro <gregpechiro@gmail.com>
2017-09-21 12:51:15 -0400

    fixed intellij reset wildcRD

--- other-upgrades/reset-intellij.sh
+++ other-upgrades/reset-intellij.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -d "/home/greg/.IntelliJIdea*/config/eval" ]; then
+if [ -e /home/greg/.IntelliJIdea*/config/eval ]; then
     echo -e '\n>>> Reseting Intellij Idea <<<'
     rm -rf /home/greg/.IntelliJIdea*/config/eval
 fi
